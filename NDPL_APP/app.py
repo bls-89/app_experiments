7581fabc91feb558b0f4faf144ea79967181703b
B S <121832699+bls-89@users.noreply.github.com>
2023-04-29 01:46:06 +0500

    Update app.py

--- NDPL_APP/app.py
+++ NDPL_APP/app.py
@@ -1,47 +1,25 @@
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
+from flask import Flask, render_template, request
 import keras
-from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-app = Flask(__name__)
 
-# Загрузка модели Keras
+app = Flask(__name__)
 model_path = '777'
 model = keras.models.load_model(model_path)
 
-# Создание объекта MinMaxScaler
-scaler = MinMaxScaler()
-scaler1 = MinMaxScaler()
-df = pd.read_csv(r'static/df_apriori.csv')
-y = df['Соотношение матрица-наполнитель']
-X = df.drop(['Unnamed: 0','Соотношение матрица-наполнитель'], axis = 1)
-
-# Определение маршрута для отображения HTML-страницы
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index1.html')
 
-# Определение маршрута для обработки данных из формы HTML
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Получение данных из формы HTML
-    input_data = request.form.to_dict()
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-    # Преобразование данных в массив numpy
-    input_array = [[float(input_data['input{}'.format(i+1)]) for i in range(12)]]
+    output = round(prediction[0][0], 2)
 
-    # Нормализация данных
-    df_nr = scaler.fit(X)
-    input_array_normalized = scaler.transform(input_array)
-    y_nr = scaler.fit(y[:,np.newaxis])
-    # Предсказание значения целевой переменной
-    prediction = model.predict(input_array_normalized)
 
-    # Обратное преобразование данных
-    prediction_unnormalized = scaler.inverse_transform(prediction)
+    return render_template('index.html', prediction_text='Predicted Variable Value: {}'.format(output))
 
-    # Возврат результата в формате JSON
-    return jsonify({'prediction': float(prediction_unnormalized[0][0])})
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
